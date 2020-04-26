99593d30eff825cc4ce33698421c45d53caa63db
Isaque Alves <isaquealves@gmail.com>
2020-04-26 13:37:12 -0300

    Adjust tests structure
    - Adjust test_app testing rules
    - Adjusts conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,47 +1,40 @@
-import os
-
+from pathlib import Path
 import pytest
+from flask import current_app
+from orator.migrations import Migrator, DatabaseMigrationRepository
+from app import create_app, get_config
+from app.providers.database import db as _db
 
-from app import create_app
-from app.settings import settings
 
 @pytest.fixture(scope='session')
 def app(request):
     """ Session wide test 'Flask' application """
-    
-    settings['TESTING'] = True
-    settings['ORATOR_DATABASES'] = {
-        'default': {
-            'driver': 'sqlite',
-            'database': '/ tmp/database.sqlite3'
-        }
-    }
 
+    settings = get_config('app.settings.Testing')
     app = create_app('local', settings)
     ctx = app.app_context()
     ctx.push()
-
     yield app
-
     ctx.pop()
 
 
 @pytest.fixture
-def client(app):
+def client(app, db):
     return app.test_client()
 
 
 @pytest.fixture(scope='session')
 def db(app, request):
     """ Session-wide test database """
-    from app.providers.database import base as _db
-    _db.drop_all()
-    _db.app = app
-    _db.create_all()
-
-    yield _db
-
-    _db.drop_all()
+    migrations_path = Path(__file__).parent.parent.joinpath('migrations')
+    with app.app_context():
+        _db.init_app(current_app)
+        repo = DatabaseMigrationRepository(_db, 'migrations')
+        migrator = Migrator(repo, _db)
+        if not migrator.repository_exists():
+            repo.create_repository()
+        migrator.rollback(migrations_path)
+        migrator.run(migrations_path)
 
 
 @pytest.fixture(scope='function')
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,17 @@
+
 import inspect
 import app
-import pudb
+
+
 def test_has_create_app():
-    assert hasattr(app, 'create_app')
+    assert hasattr(app, 'create_app')  # nosec
+
 
 def test_create_app_receive_env_args():
-    assert 'env' in inspect.signature(app.create_app).parameters.keys()
+    args = inspect.signature(app.create_app).parameters.keys()
+    assert 'env' in args  # nosec
+
 
 def test_create_app_receive_settings_args():
-    assert 'additional_settings' in inspect.signature(app.create_app).parameters.keys()
+    args = inspect.signature(app.create_app).parameters.keys()
+    assert 'config' in args  # nosec
