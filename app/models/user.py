edfd903d9b2e2816dc12adcc35db45786bcc5292
Isaque Alves <isaquealves@gmail.com>
2020-05-03 20:54:26 -0300

    Setup account activation route
    - Setup account activation route
    - Add new attribute 'active' to __fillable__ list on model

--- app/models/user.py
+++ app/models/user.py
@@ -1,30 +1,32 @@
 from typing import AnyStr
+from orator import SoftDeletes
 from app.providers.database import db
 
 
-class User(db.Model):
-    
+class User(SoftDeletes, db.Model):
+
     public_key: AnyStr
     private_key: AnyStr
 
     __table__ = 'users'
 
-    __fillable__ = ['first_name', 'last_name', 'email', 'username']
+    __fillable__ = ['first_name', 'last_name', 'email', 'username', 'active']
     __primary_key__ = 'id'
     __guarded__ = ['id', 'public_key', 'private_key']
+    __dates__ = ['deleted_at']
 
     @property
     def pub_key(self):
         return self.public_key
-    
+
     @property
     def priv_key(self):
         return self.private_key
-    
+
     @pub_key.setter
     def pub_key(self, value):
         self.public_key = value
-    
+
     @priv_key.setter
     def priv_key(self, value):
         self.private_key = value
