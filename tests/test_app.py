397eed273daa8c3a78aecf4bd5096f565773f96d
Isaque Alves <isaquealves@gmail.com>
2020-05-18 14:07:49 -0300

    Fix post data

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,11 @@
-
 import inspect
 import app
 
 
 def test_has_create_app():
-    assert hasattr(app, 'create_app')  # nosec
-
-
-def test_create_app_receive_env_args():
-    args = inspect.signature(app.create_app).parameters.keys()
-    assert 'env' in args  # nosec
+    assert hasattr(app, "create_app")  # nosec
 
 
 def test_create_app_receive_settings_args():
     args = inspect.signature(app.create_app).parameters.keys()
-    assert 'config' in args  # nosec
+    assert "config" in args  # nosec
