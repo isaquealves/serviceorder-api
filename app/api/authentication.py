4e9283557615d4ece4d939f572f1f0391510c815
Isaque Alves <isaquealves@gmail.com>
2020-05-17 23:12:43 -0300

    Add refresh functionality

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -8,7 +8,9 @@ from app.tasks import send_authentication_code
 from app.providers.redis import redisClient
 from flask_jwt_extended import (
     create_access_token,
-    create_refresh_token
+    create_refresh_token,
+    jwt_refresh_token_required,
+    get_jwt_identity,
 )
 
 
@@ -18,54 +20,45 @@ LOGGER.setLevel(logging.ERROR)
 
 def request(body: Any):
     try:
-        username = json.loads(body)['username']
+        username = json.loads(body)["username"]
         user = User.where_raw(
-            'username=? or email=?',
-            [username, username]
+            "username=? or email=?", [username, username]
         ).first()
         if user:
             send_authentication_code.apply_async((user,))
             return response_ok(
-                message='A code has been sent to your email.\
-                Please, use the code to confirm your access',
+                message="A code has been sent to your email.\
+                Please, use the code to confirm your access",
             )
     except ModuleNotFoundError as modelError:
         LOGGER.error(modelError)
     except json.JSONDecodeError as jsonError:
         LOGGER.error(jsonError)
-    return not_found_response(
-        'Auth',
-        description="user"
-    )
+    return not_found_response("Auth", description="user")
 
 
 def token(body: Any):
     try:
-        code = json.loads(body)['code'].encode()
-        stored_code, userid = redisClient.get(f'code').split('-')
+        code = json.loads(body)["code"].encode()
+        stored_code, userid = redisClient.get(code).decode().split("-")
         user = User.find(userid)
         if not stored_code == code:
-            return not_found_response(
-                'Auth',
-                description="user"
-            )
+            return not_found_response("Auth", description="user")
 
         access_token = create_access_token(user.username)
         refresh_token = create_refresh_token(user.username)
 
-        return jsonify({
-            'token': access_token,
-            'refresh': refresh_token
-        })
+        return jsonify({"token": access_token, "refresh": refresh_token})
     except ModuleNotFoundError as modelError:
         LOGGER.error(modelError)
     except json.JSONDecodeError as jsonError:
         LOGGER.error(jsonError)
-    return not_found_response(
-        'Auth',
-        description="user"
-    )
+    return not_found_response("Auth", description="user")
 
 
+@jwt_refresh_token_required
 def refresh():
-    ...
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity)
+    refresh_token = create_refresh_token(identity)
+    return jsonify({"token": access_token, "refresh": refresh_token})
