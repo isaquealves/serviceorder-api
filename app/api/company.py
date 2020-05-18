8609f3f9ab5081ea93acfbf2827fb1ba07b10552
Isaque Alves <isaquealves@gmail.com>
2020-05-18 18:30:31 -0300

    Rename methods overriding 'list' constructor

--- app/api/company.py
+++ app/api/company.py
@@ -1,14 +1,18 @@
-def list():
+def list_all():
     ...
+
 
 def get():
     ...
 
+
 def create():
     ...
 
+
 def update():
     ...
+
 
 def delete():
     ...
--- app/api/product.py
+++ app/api/product.py
@@ -1,20 +1,26 @@
-def list():
+def list_all():
     ...
+
 
 def get():
     ...
 
+
 def create():
     ...
+
 
 def update():
     ...
 
+
 def delete():
     ...
 
+
 def by_vendor():
     ...
+
 
 def byVendor():
     ...
